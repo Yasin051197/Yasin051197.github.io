1a2f60bc34ab8163cf494b6971639eaa068d1b6d
Yasin051197 <yasinjamadar0511@gmail.com>
2023-01-04 08:48:18 +0530

    added css

--- src/components/Home/ContectLogo.jsx
+++ src/components/Home/ContectLogo.jsx
@@ -10,7 +10,7 @@ const ContectLogo = () => {
   const [modalShow, setModalShow] = React.useState(false);
   const [callme, setCallme] = React.useState(false);
   return (
-    <div style={{textAlign:"unset"}}>
+    <div style={{marginLeft:"0%"}}>
       <span>
 
         <ul className="home-about-social-linksTwo">
